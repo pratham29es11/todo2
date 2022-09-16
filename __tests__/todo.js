dd06acc89a92376a66a8a3adceae6b8ab61bbe0e
pratham29es11 <72184864+pratham29es11@users.noreply.github.com>
2022-09-16 21:46:04 +0530

    commit

---
+++ __tests__/todo.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+
+describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+  test("Should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+
+  test("should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("should retreive overdue items", () => {
+    overdue();
+    expect(all.length).toBe(2);
+  });
+
+  test("should retreive due today items", () => {
+    dueToday();
+    expect(all.length).toBe(2);
+  });
+
+  test("should retreive due later items", () => {
+    dueLater();
+    expect(all.length).toBe(2);
+  });
+});
